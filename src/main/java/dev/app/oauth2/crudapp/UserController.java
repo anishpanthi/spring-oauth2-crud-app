d7b345cf3d197303706f7d8d5a807657f4c7f879
Panthi <anish.panthi41@gmail.com>
2024-01-31 16:42:52 -0600

    Completed OIDC Part

--- src/main/java/dev/app/oauth2/crudapp/UserController.java
+++ src/main/java/dev/app/oauth2/crudapp/UserController.java
@@ -1,20 +1,33 @@
 package dev.app.oauth2.crudapp;
 
-import lombok.RequiredArgsConstructor;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Anish Panthi
  */
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
+
+    private final ClientRegistration clientRegistration;
+
+    public UserController(UserService userService, ClientRegistrationRepository clientRegistrationRepository) {
+        this.userService = userService;
+        this.clientRegistration = clientRegistrationRepository.findByRegistrationId("azuread");
+    }
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAllUsers() {
@@ -42,5 +55,27 @@ public class UserController {
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteById(id);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<?> getUser(@AuthenticationPrincipal OAuth2User user) {
+        if (user == null) {
+            return new ResponseEntity<>("", HttpStatus.OK);
+        } else {
+            return ResponseEntity.ok().body(user.getAttributes());
+        }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request) {
+        // send logout URL to client so they can initiate logout
+        var issuerUri = clientRegistration.getProviderDetails().getIssuerUri();
+        var originUrl = request.getHeader(HttpHeaders.ORIGIN);
+        Object[] params = {issuerUri, clientRegistration.getClientId(), originUrl};
+        // Yes! We @ Auth0 should have an end_session_endpoint in our OIDC metadata.
+        // It's not included at the time of this writing, but will be coming soon!
+        var logoutUrl = "https://login.microsoftonline.com/1b286c40-c2fd-46d5-a553-b6502b89f42d/oauth2/v2.0/logout";
+        request.getSession().invalidate();
+        return ResponseEntity.ok().body(Map.of("logoutUrl", logoutUrl));
     }
 }
