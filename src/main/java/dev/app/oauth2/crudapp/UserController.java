953a94693c0fb3e16106e4ad5ed8b3b80e92715d
Anish <anish.panthi41@gmail.com>
2024-03-15 13:49:49 -0500

    Completed Section-2

--- src/main/java/dev/app/oauth2/crudapp/UserController.java
+++ src/main/java/dev/app/oauth2/crudapp/UserController.java
@@ -1,7 +1,6 @@
 package dev.app.oauth2.crudapp;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.List;
 import java.util.Map;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -21,43 +20,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class UserController {
-
-  private final UserService userService;
-
   private final ClientRegistration clientRegistration;
 
-  public UserController(
-      UserService userService, ClientRegistrationRepository clientRegistrationRepository) {
-    this.userService = userService;
+  public UserController(ClientRegistrationRepository clientRegistrationRepository) {
     this.clientRegistration = clientRegistrationRepository.findByRegistrationId("azuread");
-  }
-
-  @GetMapping("/users")
-  public ResponseEntity<List<User>> getAllUsers() {
-    return ResponseEntity.ok(userService.findAll());
-  }
-
-  @PostMapping("/users")
-  public ResponseEntity<Void> saveUser(@RequestBody User user) {
-    userService.save(user);
-    return ResponseEntity.ok().build();
-  }
-
-  @GetMapping("/users/{id}")
-  public ResponseEntity<User> getSingleUser(@PathVariable Long id) {
-    return ResponseEntity.ok(userService.findById(id));
-  }
-
-  @PutMapping("/users/{id}")
-  public ResponseEntity<Void> updateUser(@RequestBody User user) {
-    userService.save(user);
-    return ResponseEntity.ok().build();
-  }
-
-  @DeleteMapping("/users/{id}")
-  public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-    userService.deleteById(id);
-    return ResponseEntity.ok().build();
   }
 
   @GetMapping("/user")
@@ -80,5 +46,25 @@ public class UserController {
         "https://login.microsoftonline.com/1b286c40-c2fd-46d5-a553-b6502b89f42d/oauth2/v2.0/logout";
     request.getSession().invalidate();
     return ResponseEntity.ok().body(Map.of("logoutUrl", logoutUrl));
+  }
+
+  @GetMapping("/all/users")
+  public ResponseEntity<String> getAllUsers() {
+    var users =
+        """
+          [
+            {
+                "id": 1,
+                "name": "Anish",
+                "email": "anish@panthi.com"
+            },
+            {
+                "id": 2,
+                "name": "John",
+                "email": "John@whoisjohn.dev"
+            }
+          ]
+      """;
+    return ResponseEntity.ok().body(users);
   }
 }
